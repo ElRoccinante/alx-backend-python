4f8ee3ba1f16d9832d2833336783c7a8906e17d4
ElRoccinante <alaechouiye@gmail.com>
2024-07-05 13:06:10 +0000

    u

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""v.annotation"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """A function that uses variable annotation, takes in a
+    list of floats and returns the float sum of elements."""
+    return sum(input_list, 0.0)
