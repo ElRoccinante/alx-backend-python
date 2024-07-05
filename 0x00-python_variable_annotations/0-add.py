cef1974dadfa58aad1b1fd04f09f35bbab50e65f
ElRoccinante <alaechouiye@gmail.com>
2024-07-05 11:50:10 +0000

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""python_variable_annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """A function that uses variable annotation, to do the sum
+    of two floats."""
+    return a + b
