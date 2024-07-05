345f2a2bbb593ce9dd8070df8646de2c28c75b9d
ElRoccinante <alaechouiye@gmail.com>
2024-07-05 11:52:13 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""0x00-python_variable_annotations"""
+
+
+def floor(n: float) -> int:
+    """A function that uses variable annotation, and returns the
+    floor of a number n."""
+    return int(n)
