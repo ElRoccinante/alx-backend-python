966f028d8afb7ea840bc641385d63cd9819ee807
ElRoccinante <alaechouiye@gmail.com>
2024-07-08 20:33:32 +0000

    h

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""async coroutine"""
+""" using the random modul Write an asynchronous coroutine  """
 import asyncio
 from random import uniform
+from typing import Generator
 
-
-async def wait_random(max_delay: int = 10) -> float:
-    """async function"""
-    t = uniform(0, max_delay)
-    await asyncio.sleep(t)
-    return t
+async def async_generator() -> Generator[float, None, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
